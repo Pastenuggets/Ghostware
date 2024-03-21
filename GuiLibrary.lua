f9da3d9234090e26d34e4933889bccd0cc127c52
Monia <102583640+NTDCore@users.noreply.github.com>
2024-03-21 13:55:41 +0700

    new discord invite

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -954,8 +954,8 @@ if shared.VapeExecuted then
 						local reqbody = {
 							["nonce"] = game:GetService("HttpService"):GenerateGUID(false),
 							["args"] = {
-								["invite"] = {["code"] = "rQwt7wxpSH"},
-								["code"] = "rQwt7wxpSH",
+								["invite"] = {["code"] = "TKwneZAhTQ"},
+								["code"] = "TKwneZAhTQ",
 							},
 							["cmd"] = "INVITE_BROWSER"
 						}
